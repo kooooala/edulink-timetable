d7e5cd0198f09e386599e704d23c0eda61eeea5d
kooooala <omgthereisaleaf@gmail.com>
2024-03-18 21:07:27 +0000

    Adds getting school name

--- Edulink/Program.fs
+++ Edulink/Program.fs
@@ -1,9 +1,10 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
+module Edulink.Program
 
 open System
 open System.Net.Http
+open System.Net.Http.Headers
 open System.Text
-open System.Text.Unicode
 open Newtonsoft.Json
 open Newtonsoft.Json.Linq
 
@@ -11,32 +12,89 @@ let baseUrl = "https://www14.edulinkone.com/api/?method="
 
 let userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0"
 
-let createRequest method content =
+
+let uuid () =
+    Guid.NewGuid().ToString()
+
+let createRequestWithBaseUrl (method: string) (parameters: string) (url: string) =
     let request = new HttpRequestMessage ()
     request.Method <- HttpMethod.Post
-    request.RequestUri <- Uri (baseUrl + method)
+    request.RequestUri <- Uri (url + method)
     request.Headers.Add ("X-API-Method", method)
     request.Headers.UserAgent.ParseAdd userAgent
+    
+    let content = $$"""{
+    "jsonrpc": "2.0",
+    "method": "{{method}}",
+    "params": {
+        {{parameters}}   
+    },
+    "uuid": "{{uuid ()}}",
+    "id": "1"
+}"""
     
     request.Content <- new StringContent (content, Encoding.UTF8, "application/json")
     
     request
 
+let createRequest method parameters =
+    createRequestWithBaseUrl method parameters baseUrl
+
 let await =
     Async.AwaitTask >> Async.RunSynchronously
 
-let uuid () =
-    Guid.NewGuid().ToString()
+let sendRequest method parameters (client: HttpClient) =
+    let response =
+        createRequest method parameters
+        |> client.Send
+        
+    await (response.Content.ReadAsStringAsync())
 
-let login username password =
-    use client = new HttpClient()
+let sendAuthRequest method parameters token (client: HttpClient) =
+    let request = createRequest method parameters
+    request.Headers.Authorization <- AuthenticationHeaderValue ("Bearer", token)
+    let response = client.Send request
     
-    let body = $$"""
-{
-    "jsonrpc": "2.0",
-    "method": "EduLink.Login",
-    "params": {
-        "from_app": false,
+    await (response.Content.ReadAsStringAsync())
+
+let schoolId code =
+    use client = new HttpClient ()
+    
+    let parameters = $"""
+        "code": "{code}"
+        """
+    
+    let response =
+        createRequestWithBaseUrl "School.FromCode" parameters "https://provisioning.edulinkone.com/?method="
+        |> client.Send
+    
+    let content = await (response.Content.ReadAsStringAsync()) |> JObject.Parse
+        
+    let success = content["result"].["success"].ToObject<bool>()
+    
+    if success then
+        Some (content["result"].["school"].["school_id"].ToObject<int>())
+    else
+        None
+    
+let schoolName id =
+    use client = new HttpClient ()
+    
+    let parameters = $"""
+    "establishment_id": "{id}",
+    "from_app": false
+    """
+    
+    let response =
+        sendRequest "EduLink.SchoolDetails" parameters client
+        |> JObject.Parse
+        
+    response["result"].["establishment"].["name"].ToObject<string>()
+    
+let login username password : Auth =
+    use client = new HttpClient ()
+    
+    let parameters = $$""""from_app": false,
         "ui_info": {
             "format": 2,
             "version": "5.0.70",
@@ -46,24 +104,21 @@ let login username password =
         "device": {},
         "username": "{{username}}",
         "password": "{{password}}",
-        "establishment_id": 46
-    },
-    "uuid": "{{uuid ()}}",
-    "id": "1"
-}"""
-        
-    let response =
-        createRequest "EduLink.Login" body 
-        |> client.Send
-        
+        "establishment_id": 46"""
+       
     let content =
-        await (response.Content.ReadAsStringAsync ())
-        |> JsonConvert.DeserializeObject<JToken>
+        sendRequest "EduLink.Login" parameters client
+        |> JObject.Parse
         
-    content["result"].["authtoken"] |> string
+    content["result"].["authtoken"].ToObject<string>(), content["result"].["user"].["id"].ToObject<string>()
     
 [<EntryPoint>]
 let main _ =
-    login "19eho" "hqijbmcohvfvndqbbuuond20080407()hqijbmcohvfvndqbbuuond20080407()"
-    |> printfn "%s"
+    printfn $"""{login "19eho" "hqijbmcohvfvndqbbuuond20080407()hqijbmcohvfvndqbbuuond20080407()"}"""
+    
+    schoolId "sn14je"
+    |> function
+        | Some id -> printfn $"Name: {schoolName id}"
+        | _ -> ()
+        
     0
---
+++ Edulink/Types.fs
@@ -0,0 +1,41 @@
+﻿[<AutoOpen>]
+module Edulink.Types
+
+open System
+
+type Period = {
+    Id: int
+    Name: string
+    Start: DateTime
+    End: DateTime
+    Empty: bool
+}
+
+type Room = {
+    Name: string
+    Moved: bool
+}
+
+type TeachingGroup = {
+    Name: string
+    Subject: string
+}
+
+type Lesson = {
+    PeriodId: int
+    Room: Room
+    TeachingGroup: TeachingGroup
+    Teachers: string
+}
+
+type Day = {
+    Date: DateTime
+    Periods: Period list
+    Lessons: Lesson list
+}
+
+type Week = Day list
+
+type Weeks = Week list
+
+type Auth = string * string
